2247be6c251f15ff76b02bf1853677d3d6638747
valeriy mixailov <ethno4ka@gmail.com>
2020-05-23 01:33:39 +0300

    Adding Favourite section

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -3,8 +3,8 @@ import './Layout.css';
 
 const Layout = () => {
   return (
-    <div className="d-flex flex-row justify-content-around m-5">
-      <div className="d-flex flex-column col-5">
+    <div className="d-flex flex-row justify-content-between pl-5">
+      <div className="d-flex flex-column col-5 m-5 ">
         <h1>Hey!</h1>
         <h2>Let's try to find a joke for you:</h2>
         <div className="d-flex flex-column mt-3">
@@ -83,7 +83,29 @@ const Layout = () => {
           <div className="categorie align-self-end">CELEBRITY</div>
         </div>
       </div>
-      <div className="sidebar d-flex flex-column col-5"></div>
+      <div className="sidebar d-flex flex-column col-4 pt-5">
+        <h2>Favourite</h2>
+        <div className="card favourite-card mt-5 p-4">
+          <i className="far fa-heart col-1 align-self-end"></i>
+          <div className="card-body d-flex justify-content-between">
+            <i class="far fa-comment-alt fa-inverse col-1 mr-1"></i>
+            <p className="col-11">
+              No one truly knows who's Chuck Norris' real father. No one is
+              biologically strong enough for this. He must've conceived himself.
+            </p>
+          </div>
+        </div>
+        <div className="card favourite-card mt-5 p-4">
+          <i className="far fa-heart col-1 align-self-end"></i>
+          <div className="card-body d-flex justify-content-between">
+            <i class="far fa-comment-alt fa-inverse col-1 mr-1"></i>
+            <p className="col-11">
+              No one truly knows who's Chuck Norris' real father. No one is
+              biologically strong enough for this. He must've conceived himself.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
